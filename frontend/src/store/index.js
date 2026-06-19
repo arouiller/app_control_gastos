9a5af62fd2acba6561681bfd3dd058843a9c890c
arouiller <alejandro.rouiller@gmail.com>
2026-06-18 21:30:05 -0300

    store: register gastoBorradores slice in redux store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
 import expensesReducer from './expensesSlice'
 import categoriesReducer from './categoriesSlice'
+import gastoBorradoresReducer from './gastoBorradoresSlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     expenses: expensesReducer,
     categories: categoriesReducer,
+    gastoBorradores: gastoBorradoresReducer,
   },
 })
 
